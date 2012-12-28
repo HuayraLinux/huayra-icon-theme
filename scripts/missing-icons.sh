3b1018004cebfe055cc6bda86d5f124a6e5ddb71
Mauro <mauro@cacavoladora.org>
2012-12-27 23:54:13 -0300

    script para ver los iconos faltantes

---
+++ scripts/missing-icons.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+mayo_vs_verde(){
+
+    for i in `ls huayra-mayo`;
+    do 
+        echo "$i: `[ -f huayra-fresco-verde/scalable/all/$i ]||echo -e "no "`existe"; 
+    done
+}
+
+verde_vs_mayo(){
+
+    for i in `ls huayra-fresco-verde`;
+    do 
+        echo "$i: `[ -f huayra-mayo/scalable/all/$i ]||echo -e "no "`existe"; 
+    done
+}
+
+violeta_vs_mayo(){
+    for ii in `find huayra -type f`;
+    do 
+        i=`basename $i`;
+        echo "$i: `[ -f huayra-mayo/scalable/all/$i ]||echo -e "no "`existe"; 
+    done
+}
+
+violeta_vs_verde(){
+    for ii in `find huayra -type f`;
+    do 
+        i=`basename $ii`;
+        echo "$i: `[ -f huayra-fresco-verde/scalable/all/$i ]||echo -e "no "`existe"; 
+    done
+}
+
+[ -z $1 ] && exit 0 || $1;
